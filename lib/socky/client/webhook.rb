3769bd583766a91d2c3c8bb9790acc8546c907bd
Julian Schuh <julez@julez.in>
2012-07-27 11:32:46 +0200

    Added Debug Output

--- lib/socky/client/webhook.rb
+++ lib/socky/client/webhook.rb
@@ -12,6 +12,8 @@ module Socky
         request.body.rewind
         @body = request.body.read
         request.body.rewind
+
+        puts 'hash: ' + @hash.to_s + ' body: ' + @body.to_s
       end
 
       # Determines if the received webhook data is valid by checking the hash
@@ -20,6 +22,7 @@ module Socky
 		  return false if @hash.nil? || @body.nil?
 
 		  salt, expected_hash = @hash.split(':',2)
+		  puts 'salt: ' + salt.to_s + ' exp: ' + expected_hash.to_s
 
 		  data_to_sign = [salt, @body].collect(&:to_s).join(':')
 
